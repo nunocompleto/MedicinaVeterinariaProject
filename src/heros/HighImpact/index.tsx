43ae83eebbe58970843cf0cb20e632fd893446fd
nunocompleto <nunocompletokhn@gmail.com>
2026-05-05 19:43:01 +0100

    2nd commitz

---
+++ src/heros/HighImpact/index.tsx
@@ -0,0 +1,47 @@
+'use client'
+import { useHeaderTheme } from '@/providers/HeaderTheme'
+import React, { useEffect } from 'react'
+
+import type { Page } from '@/payload-types'
+
+import { CMSLink } from '@/components/Link'
+import { Media } from '@/components/Media'
+import RichText from '@/components/RichText'
+
+export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
+  const { setHeaderTheme } = useHeaderTheme()
+
+  useEffect(() => {
+    setHeaderTheme('dark')
+  })
+
+  return (
+    <div
+      className="relative -mt-[10.4rem] flex items-center justify-center text-white"
+      data-theme="dark"
+    >
+      <div className="container mb-8 z-10 relative flex items-center justify-center">
+        
+        <div className="w-full md:text-center p-6 md:p-6 rounded">
+          {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
+          {Array.isArray(links) && links.length > 0 && (
+            <ul className="flex md:justify-center gap-4">
+              {links.map(({ link }, i) => {
+                return (
+                  <li key={i}>
+                    <CMSLink {...link} />
+                  </li>
+                )
+              })}
+            </ul>
+          )}
+        </div>
+      </div>
+      <div className="min-h-[60vh] select-none">
+        {media && typeof media === 'object' && (
+          <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
+        )}
+      </div>
+    </div>
+  )
+}
