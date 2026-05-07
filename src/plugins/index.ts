3477645c00c3a69464fc7a413bd919bf67faa01b
nunocompleto <nunocompletokhn@gmail.com>
2026-05-07 21:47:42 +0100

    Vercel blob adapter for a 6th time

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -9,7 +9,7 @@ import { GenerateTitle, GenerateURL } from '@payloadcms/plugin-seo/types'
 import { FixedToolbarFeature, HeadingFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
 import { searchFields } from '@/search/fieldOverrides'
 import { beforeSyncWithSearch } from '@/search/beforeSync'
-
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { Page, Post } from '@/payload-types'
 import { getServerSideURL } from '@/utilities/getURL'
 
