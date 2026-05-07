8d2c4116832818dd50f7478e750fc55322dda58c
nunocompleto <nunocompletokhn@gmail.com>
2026-05-07 16:21:19 +0100

    add postgres migrations2

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260507_145924 from './20260507_145924';
+import * as migration_20260507_151950 from './20260507_151950';
 
 export const migrations = [
   {
     up: migration_20260507_145924.up,
     down: migration_20260507_145924.down,
-    name: '20260507_145924'
+    name: '20260507_145924',
+  },
+  {
+    up: migration_20260507_151950.up,
+    down: migration_20260507_151950.down,
+    name: '20260507_151950'
   },
 ];
