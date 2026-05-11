73c86e1afef630a635ed9bd19fabd1518cc32f93
nunocompleto <nunocompletokhn@gmail.com>
2026-05-11 12:19:16 +0100

    add sticky navbar and logo

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -30,7 +30,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   }, [headerTheme])
 
   return (
-    <header className="container relative z-20" {...(theme ? { 'data-theme': theme } : {})}>
+    <header className="sticky top-0 z-50 bg-background container" {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-8 flex justify-between">
         <Link href="/">
           <Logo loading="eager" priority="high" className="dark:invert-1" />
