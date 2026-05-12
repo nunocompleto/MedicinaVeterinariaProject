dca7cdcdc4578fa00c708f94eec194c86a5a0558
nunocompleto <nunocompletokhn@gmail.com>
2026-05-12 12:28:48 +0100

    nav bar with dropdown 2nd commit

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260507_145924 from './20260507_145924';
 import * as migration_20260507_151950 from './20260507_151950';
+import * as migration_20260512_112832 from './20260512_112832';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260507_151950.up,
     down: migration_20260507_151950.down,
-    name: '20260507_151950'
+    name: '20260507_151950',
+  },
+  {
+    up: migration_20260512_112832.up,
+    down: migration_20260512_112832.down,
+    name: '20260512_112832'
   },
 ];
